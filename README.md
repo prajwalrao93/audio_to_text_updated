c4ada0940bc618e96f2c7c431f21faf71929bacc
prajwalrao93 <34549044+prajwalrao93@users.noreply.github.com>
2020-05-03 22:02:25 +0530

    Updated the code and updated readme

--- README.md
+++ README.md
@@ -1,146 +1,37 @@
-"# audio_to_text_updated" 
-# -*- coding: utf-8 -*-
-# Form implementation generated from reading ui file 'speech_to_text.ui'
-#
-# Created by: PyQt5 UI code generator 5.9.2
+"# audio_to_text_updated"
 
-import sys
-from PyQt5 import QtCore, QtGui, QtWidgets
-import os, langid
-from os import listdir
-import speech_recognition as sr
-from pydub import AudioSegment
-from wit import WIT
-
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        
-        MainWindow.resize(640, 480)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(20, 110, 131, 41))
-        font = QtGui.QFont()
-        font.setPointSize(16)
-        self.label.setFont(font)
-        self.label.setObjectName("label")
-        
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(20, 170, 131, 41))
-        font = QtGui.QFont()
-        font.setPointSize(16)
-        self.label_2.setFont(font)
-        self.label_2.setObjectName("label_2")
-
-        
-        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(160, 120, 331, 31))
-        self.lineEdit.setObjectName("lineEdit")
-        
-        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_2.setGeometry(QtCore.QRect(160, 180, 331, 31))
-        self.lineEdit_2.setObjectName("lineEdit_2")
-
-        self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(160, 230, 131, 41))
-        font = QtGui.QFont()
-        font.setPointSize(16)
-        self.label_3.setFont(font)
-        self.label_3.setObjectName("label_2")
-        
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(160, 280, 121, 41))
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        self.pushButton.setFont(font)
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton.clicked.connect(self.submit)
-        
-        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(350, 280, 141, 41))
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        self.pushButton_2.setFont(font)
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.pushButton_2.clicked.connect(QtCore.QCoreApplication.instance().quit)
-
-        
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 640, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "Path to Media: "))
-        self.label_2.setText(_translate("MainWindow", "Path to Excel: "))
-        self.pushButton.setText(_translate("MainWindow", "Submit"))
-        self.pushButton_2.setText(_translate("MainWindow", "Exit"))
+Audio format conversion (to .wav format) using FFmpeg
+Audio to text conversion using Python
 
 
-
-    def submit(self,MainWindow):
-        
-        mp3_path = os.path.normpath(self.lineEdit.text())
-        excel_path = os.path.normpath(self.lineEdit_2.text())
-        files = os.listdir(mp3_path)
-
-        mp3_files = [x for x in files if ".mp3" in x]
-
-        for file_name in mp3_files:
-            file = os.path.join(mp3_path, file_name)
-            song = AudioSegment.from_mp3(file)
-            file = file.replace(".mp3", ".wav")
-            song.export(file, format="wav")
-            
-            r = sr.Recognizer()
-            file_audio = sr.AudioFile(file)
-            with file_audio as source:
-                r.adjust_for_ambient_noise(source)
-                audio_text = r.record(source)
-                
-            WIT_AI_KEY = "JS3HNTZH22MFTKO24UA5QRMELMPDMAUO" # Wit.ai keys are 32-character uppercase alphanumeric strings
-            try:
-                text = r.recognize_sphinx(audio_text)
-                lang = langid.classify(text)[0]
-                print(lang)
-                text = r.recognize_wit(audio_text, key=WIT_AI_KEY)
-
-                txt_file = file.replace(".wav", ".txt")
-                with open(txt_file, 'w') as out:
-                    out.write(text)
-
-                os.remove(file)
-            except sr.UnknownValueError:
-                print("Wit.ai could not understand audio")
-            except sr.RequestError as e:
-                print("Could not request results from Wit.ai service; {0}".format(e))
-                
-
-        self.msg = QtWidgets.QMessageBox(self.centralwidget)
-        self.msg.setIcon(QtWidgets.QMessageBox.Information)
-
-        self.msg.setText("Completed")
-        self.msg.setWindowTitle("Completed")
-        self.msg.setStandardButtons(QtWidgets.QMessageBox.Ok)       
-        self.msg.exec_()
-        
+Packages used (for exact versions used refer requirements.txt):
+1. tkinter and ttkthemes for the GUI application
+2. threading to make the gui interactive
+3. openpyxl to work with excel files
+4. speechRecognition and wit for the audio to text conversion
 
 
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
+API used for conversion
+1. wit.ai - open source api (https://wit.ai/faq)
+
+
+Build the executable file of the application using "cx_freeze" package. Run the command "python setup.py build" using the command line. Execuble file can be found inside "build\exe.win-amd64-3.8" folder.
+
+
+Steps to follow while using the app
+1. Place the media files in the Media folder.
+2. Run the Converter.bat file in the Media folder and wait for it to get completed.
+3. Once the bat file has run, now run the speech_to_text.exe file.
+4. Copy the folder path of the media folder and paste in the first open space (or click the button next to open box and select the folder)
+5. Copy the raw data file path along with file name in the second open space (or click the button next to open box and select the file)
+6. Click on Submit button. Wait for the application to run and complete
+7. Use Clear button to clear the fields
+8. Use Exit button to exit the application
+9. Use Help button for help on how to use the application
+
+
+Challenges faced currently (to be updated if there is any):
+1. Reducing the noise in the audio file
+2. After trimming the silent part in the audio file, the updated audio is not getting uploaded to speech_recognition package and the program crashes.
+3. Audio conversion task needs to be done seperately, cannot be included in the main script because command line interface keeps popping up for every file converted.
+4. Lots of "Request Timeout" (Updated the message to "Audio not Recognized". Exact reason unknown, possible reasons due to wit server down, slow network connection, bad audio quality)
--- speech_to_text.py
+++ speech_to_text.py
@@ -1,20 +1,65 @@
 from tkinter import *
 from tkinter.ttk import *
-from tkinter import messagebox
+from tkinter import messagebox, filedialog
 from tkinter import font
 from ttkthemes import ThemedTk
 from os import listdir
-import os, openpyxl, sys, time, threading, queue
+import os, openpyxl, sys, time, threading, shutil
 import speech_recognition as sr
 from ffmpy import FFmpeg
+from tkinter.scrolledtext import ScrolledText
+from wit import Wit
+import noisereduce as nr
+
+
+class helpScreen:
+    def __init__(self, root):
+        self.root = root
+        self.root.minsize(500,500)
+        self.root.resizable(False, False)
+        self.root['bg'] = "#300a24"
+        self.style = Style()
+        self.style.theme_use("radiance")
+        self.style.configure('TButton', background="#300a24")
+        self.style.configure('TFrame', background="#300a24")
+        self.style.configure('TLabel', foreground='white', background="#300a24")
+        self.style.configure('TEntry', background="#300a24")
+        
+          
+        self.font_def = font.Font(family="Helvetica", size=12, weight='bold', slant='italic')
+
+        text = """
+Method to use this app
+
+1: Place the media files in the Media folder.
+2: Run the Converter.bat file in the Media folder and wait for it to get completed.
+3: Once the bat file has run, now run the speech_to_text.exe file.
+4: Copy the folder path of the media folder and paste in the first open space
+5: Copy the raw data file path along with file name in the second open space
+6: Click on Submit button. Wait for the application to run and complete
+7: Use Clear button to clear the fields
+8: Use Exit button to exit the application
+9: Use Help button for help on how to use the application
+
+For more information contact
+"""
+
+        self.frame = Frame(self.root, style="TFrame")
+
+        self.text1 = ScrolledText(self.frame, wrap=WORD, width=30, height=20)
+        self.text1.pack(expand=True, side=TOP, padx=10, pady=10)
+        self.text1.insert(INSERT, text)
+
+        self.button = Button(self.frame, text="Quit", width=10, command=self.root.destroy, style="TButton")
+        self.button.pack(expand=True, side=TOP)
+
+        self.frame.pack(expand=True)
+        
 
 
 class MyApp():
     def __init__(self, root):
         self.root = root
-        self.queue = queue.Queue()
-        #self.w_theme = ThemedStyle(self.root)
-        #self.w_theme.set_theme("Black")
         self.style = Style()
         self.style.theme_use("radiance")
         self.style.configure('TButton', background="#300a24")
@@ -25,7 +70,7 @@ class MyApp():
         self.root.minsize(500,500)
         self.root.resizable(False, False)
         self.root['bg'] = "#300a24"
-
+        
         self.font_def = font.Font(family="Helvetica", size=12, weight='bold', slant='italic')
 
         self.frame1 = Frame(root, style="TFrame")
@@ -41,11 +86,17 @@ class MyApp():
         self.entry1 = Entry(self.frame2, width=40, style="TEntry")
         self.entry1.pack(expand=True, side=LEFT, padx=50)
 
+        self.button5 = Button(self.frame2, text="...", style="TButton", width=2, command=self.select_directory)
+        self.button5.pack(expand=True, side=LEFT, padx=50)
+
         self.label2 = Label(self.frame3, text="Raw Data File Path: ", style="TLabel", font=self.font_def)
         self.label2.pack(expand=True, side=LEFT, padx=50)
 
         self.entry2 = Entry(self.frame3, width=40, style="TEntry")
         self.entry2.pack(expand=True, side=LEFT, padx=50)
+
+        self.button6 = Button(self.frame3, text="...", style="TButton", width=2, command=self.select_file)
+        self.button6.pack(expand=True, side=LEFT, padx=50)
 
         self.button1 = Button(self.frame4, text="Submit", style="TButton", width=10, command=self.thread_func)
         self.button1.pack(expand=True, side=LEFT, padx=20, pady=10)
@@ -63,8 +114,13 @@ class MyApp():
         self.name = StringVar()
         self.name.set("")
 
+
         self.label3 = Label(self.frame6, textvariable=self.name, style="TLabel", font=self.font_def)
-        self.label3.pack(expand=True)
+        self.label3.pack(expand=True, side=TOP)
+
+        self.button4 = Button(self.frame6, text="Help", width=10, style="TButton", command=self.second_window)
+        self.button4.pack(expand=True, side=TOP)
+
 
         self.frame1.pack(expand=True)
         self.frame2.pack(expand=True)
@@ -73,11 +129,35 @@ class MyApp():
         self.frame5.pack(expand=True)
         self.frame6.pack(expand=True)
 
+    def select_directory(self):
+        self.wav_path = filedialog.askdirectory()
+        self.entry1.delete(0, END)
+        self.entry1.insert(0, self.wav_path)
+        self.entry1.update
+
+    def select_file(self):
+        self.excel_path = filedialog.askopenfilename(filetypes=(("excel files", ".xlsx"), ("excel files", ".xlsm"),("all files",".")))
+        self.entry2.delete(0, END)
+        self.entry2.insert(0, self.excel_path)
+        self.entry2.update
+
+    def second_window(self):
+        self.root1 = ThemedTk(theme="radiance")
+        self.helpWindow = helpScreen(self.root1)
+        self.root1.mainloop()
+
+
     def clearAll(self):
         self.entry1.delete(0, last=len(self.entry1.get()))
         self.entry1.update
         self.entry2.delete(0, last=len(self.entry2.get()))
         self.entry2.update
+
+
+    #def trim_silence(self, file):
+        #y, sr = librosa.load(file)
+        #y_trimmed, index = librosa.effects.trim(y, top_db=20, frame_length=2, hop_length=500)
+        #librosa.output.write_wav(file, y_trimmed, sr)
 
 
     def submit(self):
@@ -87,13 +167,22 @@ class MyApp():
         elif self.entry2.get() == "":
             messagebox.showerror("Error", "Please provide the path to Raw Data file")
             self.button1.config(state="enabled")
+        elif '\\' not in self.entry1.get():
+            messagebox.showerror("Error", "Please provide correct path to Media")
+            self.button1.config(state="enabled")
+        elif ".xlsx" not in self.entry2.get():
+            messagebox.showerror("Error", "Please provide correct path to Raw Data file")
+            self.button1.config(state="enabled")
         else:
             mp3_path = os.path.normpath(self.entry1.get())
             excel_path = os.path.normpath(self.entry2.get())
             wit_ai_key = "WWEHIPV5SE6EXEAV6YE4QAJZE5LF22JW"
 
+            os.mkdir(os.path.join(mp3_path, NotRecognized))
+
             files = os.listdir(mp3_path)
-            mpeg4_files = [x for x in files if '.mpeg4' in x]
+            mpeg4_files = [x for x in files if '.wav' in x]
+
 
             wb1 = openpyxl.load_workbook(excel_path, keep_vba=True)
             wb2 = openpyxl.Workbook()
@@ -106,8 +195,11 @@ class MyApp():
 
             row_num = 2
             j = True
+            file_num = 1
 
-            while (j):        
+            while (j):
+                time.sleep(1)
+
                 intnr = int(sheet1.cell(row=row_num, column=1).value)
                 sheet2.cell(row=row_num, column=1).value = intnr
                 sheet2.cell(row=row_num, column=2).value = sheet1.cell(row=row_num, column=2).value
@@ -118,48 +210,48 @@ class MyApp():
                 u = 0
 
                 for u,v in enumerate(file_name):
-                    self.pbar['value'] = 100 * (mpeg4_files.index(v) + 1) / len(mpeg4_files)
+                    self.pbar['value'] = 100 * (file_num) / len(mpeg4_files)
                     self.name.set(f"Processing File: {v}")
-                    self.root.update
-                    
-                    file = v.replace(".mpeg4", ".wav")
+                    self.root.update()
 
-                    d = os.getcwd()
-                    ff = FFmpeg(executable=d + '/ffmpeg/bin/ffmpeg.exe',
-                            inputs={os.path.join(mp3_path,v): None},
-                            outputs={file: None})
-                    command = f'ffmpeg -i {os.path.join(mp3_path,v)} {file}'
+                    #self.trim_silence(os.path.join(mp3_path, v))
 
-                    ff.cmd
-                    f'ffmpeg -i {os.path.join(mp3_path,v)} {file}'
-                    ff.run()
+                    #client = Wit(wit_ai_key)
+                    #resp = None
+                    #with open(os.path.join(mp3_path, v)) as source:
+                        #try:
+                            #resp = client.speech(source, None, {'Content-Type':'audio/wav'})
+                        #except:
+                            #resp = {'_text':""}
 
-                    #r = sr.Recognizer()
-                    #with sr.AudioFile(file) as source:
-                        # r.adjust_for_ambient_noise(source)
-                        #audio_text = r.listen(source)
-
-                    #try:
-                        #text = r.recognize_wit(audio_text, key=wit_ai_key)
-                    #except sr.UnknownValueError:
-                        #text = "Couldn't Recognize"
-                    #except sr.RequestError as e:
-                        #text = "Request Timeout"
-                    text = "xyz"
-
+                    r = sr.Recognizer()
+                    with sr.AudioFile(os.path.join(mp3_path, v)) as source:
+                        r.adjust_for_ambient_noise(source)
+                        audio_text = r.listen(source)
+                    #text = str(resp['_text'])
+                    try:
+                        text = r.recognize_wit(audio_text, key=wit_ai_key)
+                    except sr.UnknownValueError:
+                        text = "Audio not Clear"
+                        shutil(os.path.join(mp3_path, v), os.path.join(mp3_path, NotRecognized, v))
+                    except sr.RequestError as e:
+                        text = "Audio not Recognized"
+                        shutil(os.path.join(mp3_path, v), os.path.join(mp3_path, NotRecognized, v))
 
                     if row_num == 2:
                         sheet2.cell(row=1, column=u * 2 + 5).value = "File Name"
                         sheet2.cell(row=1, column=u * 2 + 6).value = "Text"
-                        sheet2.cell(row=row_num, column=u * 2 + 5).value = v
-                        sheet2.cell(row=row_num, column=u * 2 + 5).hyperlink = os.path.join(mp3_path, v)
+                        sheet2.cell(row=row_num, column=u * 2 + 5).value = v.replace(".wav", "")
+                        sheet2.cell(row=row_num, column=u * 2 + 5).hyperlink = os.path.join(mp3_path, v.replace(".wav", ".mpeg4"))
                         sheet2.cell(row=row_num, column=u * 2 + 6).value = text
                     else:
-                        sheet2.cell(row=row_num, column=u * 2 + 5).value = v
-                        sheet2.cell(row=row_num, column=u * 2 + 5).hyperlink = os.path.join(mp3_path, v)
+                        sheet2.cell(row=row_num, column=u * 2 + 5).value = v.replace(".wav", "")
+                        sheet2.cell(row=row_num, column=u * 2 + 5).hyperlink = os.path.join(mp3_path, v.replace(".wav", ".mpeg4"))
                         sheet2.cell(row=row_num, column=u * 2 + 6).value = text
 
-                    os.remove(file)
+                    os.remove(os.path.join(mp3_path, v))
+                    file_num += 1
+                    
 
                 row_num += 1
                 if sheet1.cell(row=row_num, column=1).value == None:
