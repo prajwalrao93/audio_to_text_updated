623f3fc047307dc287932b737b048ec4d0b0c629
prajwalrao93 <34549044+prajwalrao93@users.noreply.github.com>
2020-05-03 23:27:35 +0530

    setup file updated

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from cx_Freeze import setup, Executable
+import os, sys
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-buildOptions = dict(packages = [], excludes = [])
 
-import sys
+where = os.path.dirname(sys.executable)
+
+
+#os.environ['TCL_LIBRARY'] = where+"\\tcl\\tcl8.6"
+#os.environ['TK_LIBRARY'] = where+"\\tcl\\tk8.6"
+
+#includefiles=["FFmpeg","venv/tcl/tcl8.6", "venv/tcl/tk8.6"]
+includefiles=["Media"]
+
+buildOptions = dict(packages = ['scipy'], excludes = [], include_files=includefiles)
+
 base = 'Win32GUI' if sys.platform=='win32' else None
 
 executables = [
